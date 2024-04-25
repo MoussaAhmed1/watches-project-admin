5898e83b4d91223e48ad39b678c36761c8d9542d
Mousa Ahmed <moussa.abdelghany@gmail.com>
2024-04-25 09:07:32 +0200

    feat/session apply

--- actions/users.ts
+++ actions/users.ts
@@ -4,13 +4,14 @@
 
 import { cookies } from "next/headers";
 import { revalidatePath } from "next/cache";
-
+import { getServerSession } from "next-auth";
 import axiosInstance, {
   Params,
   endpoints,
   getErrorMessage,
 } from "../utils/axios-client";
 import { ITEMS_PER_PAGE } from "./Global-variables";
+import { authOptions } from "@/app/api/auth/_options";
 
 interface IParams {
   user_id?: string;
@@ -25,7 +26,10 @@ export const fetchUsers = async ({
   headers,
 }: Params): Promise<any> => {
   const lang = cookies().get("Language")?.value;
-
+  const accessToken = cookies().get('accessToken')?.value;
+  const session = await getServerSession(authOptions);
+  console.log("--------------session",session)
+  console.log("--------------accessToken",accessToken)
   try {
     const res = await axiosInstance(endpoints.users.fetch, {
       params: {
@@ -35,12 +39,13 @@ export const fetchUsers = async ({
         sortBy: "created_at=desc",
       },
       headers: {
-        Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwic3ViIjoiMSIsImlhdCI6MTcxMzk5MDIzNywiZXhwIjoxNzI5NTQyMjM3fQ.zNGeaHHTEMexUf45OYIk8RUjOdf9r153qfWE2BjxlIA`,
+        Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Iis5Njc1NTU1NTg4OCIsInN1YiI6IjBlM2E0MzJjLWZhN2ItNDA5Ni05NDAwLWI5MzYzZTljMjM0NCIsImlhdCI6MTcxNDAyNjQzMiwiZXhwIjoxNzI5NTc4NDMyfQ.j9FfUYuiguiCO_MThlD8eAycYT5e-uRT3lSbLsb4IAM`,
         "Accept-Language": lang,
       },
     });
     return res;
   } catch (error: any) {
+    console.log(error)
     return {
       error: getErrorMessage(error),
     };
