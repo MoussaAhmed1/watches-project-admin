bff8ce5bb7e75eae014ac3b2c1f3f275a3ef5fc9
Omar Mohamed <70483549+omarragab1948@users.noreply.github.com>
2024-04-25 14:46:35 +0200

    fix height

--- components/ui/heading.tsx
+++ components/ui/heading.tsx
@@ -1,13 +1,22 @@
 interface HeadingProps {
   title: string;
   description?: string;
+  customStyle?: string;
 }
 
-export const Heading: React.FC<HeadingProps> = ({ title, description }) => {
+export const Heading: React.FC<HeadingProps> = ({
+  title,
+  description,
+  customStyle,
+}) => {
   return (
     <div>
-      <h2 className="text-3xl font-bold tracking-tight">{title}</h2>
-      {description &&<p className="text-sm text-muted-foreground">{description}</p>}
+      <h2 className={`text-3xl font-bold tracking-tight ${customStyle}`}>
+        {title}
+      </h2>
+      {description && (
+        <p className="text-sm text-muted-foreground">{description}</p>
+      )}
     </div>
   );
 };
