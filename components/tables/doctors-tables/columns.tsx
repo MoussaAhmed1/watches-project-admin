e6d9c3e89f35e538633a99f40b7a6a65e8bd6c1f
Mousa Ahmed <moussa.abdelghany@gmail.com>
2024-04-25 11:29:10 +0200

    Update columns.tsx

--- components/tables/doctors-tables/columns.tsx
+++ components/tables/doctors-tables/columns.tsx
@@ -29,10 +29,10 @@ export const columns: ColumnDef<IDoctor>[] = [
     header: "Rating",
     cell: ({ row }) => <div className="stars flex">
       {Array.from({ length: Math.ceil(row?.original?.rating) }, (ele, index) => (
-        <Star key={index} fill="#f7d722" strokeWidth={0} />
+        <Star key={index} fill="#f7d722" strokeWidth={0} size={20} />
       ))}
       {Array.from({ length: Math.ceil(5 - row?.original?.rating) }, (ele, index) => (
-        <Star key={index} fill="#111" strokeWidth={1} stroke="#fff" />
+        <Star key={index} fill="#111" strokeWidth={1} stroke="#fff" size={20} />
       ))}
 
     </div>
