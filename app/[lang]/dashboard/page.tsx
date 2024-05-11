69e93326e186d8e37ed8a0409441dd98d487eb6d
Mousa Ahmed <moussa.abdelghany@gmail.com>
2024-05-11 04:37:39 +0300

    feat/statistics

--- app/[lang]/dashboard/page.tsx
+++ app/[lang]/dashboard/page.tsx
@@ -1,3 +1,6 @@
+'use server';
+
+import { fetchStatictics } from "@/actions/statictics";
 import {
   Card,
   CardContent,
@@ -6,49 +9,50 @@ import {
 } from "@/components/ui/card";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Tabs, TabsContent } from "@/components/ui/tabs";
-const statistics = [
-  {
-    title: "doctors",
-    total:10
-  },
-  {
-    title: "patients",
-    total:10
-  },
-  {
-    title: "Pharmacies",
-    total:10
-  },
-  {
-    title: "Radiographer and X-ray",
-    total:10
-  },
-  {
-    title: "Sonar Doctor",
-    total:10
-  },
-  {
-    title: "Labs",
-    total:10
-  },
-  {
-    title: "Health Care Providers",
-    total:10
-  },
-  {
-    title: "Hospitals",
-    total:10
-  },
-  {
-    title: "Reservations",
-    total:10
-  },
-  {
-    title: "profit",
-    total:10
-  },
-]
-export default function page() {
+import { Statictic } from "@/types/statictic";
+import Link from "next/link";
+export default async function page({ params }: { params: { lang: string } }) {
+  const res = await fetchStatictics({ lang: params?.lang });
+  const statistics_res: Statictic = res?.data?.data;
+  const statistics = [
+    {
+      title: "Patients",
+      total: statistics_res?.clients_count,
+      link: "/dashboard/patients"
+    },
+    {
+      title: "Doctors",
+      total: statistics_res?.doctors_count,
+      link: "/dashboard/doctors"
+    },
+    {
+      title: "Nurses",
+      total: statistics_res?.nurses_count,
+      link: "/dashboard/nurses"
+    },
+    {
+      title: "Pharmacies",
+      total: statistics_res?.pharmacy_count,
+      link: "/dashboard/pharmacies"
+    },
+    {
+      title: "Pharmacy Orders",
+      total: statistics_res?.pharamcy_order_count,
+      link: '/dashboard/pharmacy-orders'
+    },
+    {
+      title: "Nurse Orders",
+      total: statistics_res?.nurse_order_count,
+      link: '/dashboard/nurse-orders'
+    },
+
+    {
+      title: "Reservations",
+      total: statistics_res?.reservations_count,
+      link: "/dashboard/reservations"
+    },
+  ]
+
   return (
     <ScrollArea className="h-full">
       <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
@@ -63,18 +67,20 @@ export default function page() {
 
               {statistics.map(statistic => (
 
-                <Card key={statistic?.title}>
-                  <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                    <CardTitle className="text-sm font-medium capitalize">
-                      {statistic?.title}
-                    </CardTitle>
-                    {/* {statistic?.icon} */}
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 28 28"><path fill="#8c8c8c" d="M15 8a1 1 0 1 1-2 0a1 1 0 0 1 2 0m-1.75 3.75v7.5a.75.75 0 0 0 1.5 0v-7.5a.75.75 0 0 0-1.5 0M2 14C2 7.373 7.373 2 14 2s12 5.373 12 12s-5.373 12-12 12S2 20.627 2 14M14 3.5C8.201 3.5 3.5 8.201 3.5 14S8.201 24.5 14 24.5S24.5 19.799 24.5 14S19.799 3.5 14 3.5"/></svg>
-                  </CardHeader>
-                  <CardContent>
-                    <div className="text-2xl font-bold">{statistic.total}</div>
-                  </CardContent>
-                </Card>
+                <Link href={statistic?.link} key={statistic?.title}>
+                  <Card  className="cursor-pointer">
+                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                      <CardTitle className="text-sm font-medium capitalize">
+                        {statistic?.title}
+                      </CardTitle>
+                      {/* {statistic?.icon} */}
+                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 28 28"><path fill="#8c8c8c" d="M15 8a1 1 0 1 1-2 0a1 1 0 0 1 2 0m-1.75 3.75v7.5a.75.75 0 0 0 1.5 0v-7.5a.75.75 0 0 0-1.5 0M2 14C2 7.373 7.373 2 14 2s12 5.373 12 12s-5.373 12-12 12S2 20.627 2 14M14 3.5C8.201 3.5 3.5 8.201 3.5 14S8.201 24.5 14 24.5S24.5 19.799 24.5 14S19.799 3.5 14 3.5" /></svg>
+                    </CardHeader>
+                    <CardContent>
+                      <div className="text-2xl font-bold">{statistic.total}</div>
+                    </CardContent>
+                  </Card>
+                </Link>
               ))
               }
 
---
+++ types/statictic.ts
@@ -0,0 +1,10 @@
+export interface Statictic {
+    doctors_count: number
+    clients_count: number
+    nurses_count: number
+    pharmacy_count: number
+    reservations_count: number
+    nurse_order_count: number
+    pharamcy_order_count: number
+  }
+  
