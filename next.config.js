42e4e94c287d9755ca87c4cfe6a858c7e2d7bb03
Mousa Ahmed <moussa.abdelghany@gmail.com>
2024-05-17 11:37:45 +0300

    show images

--- next.config.js
+++ next.config.js
@@ -4,16 +4,8 @@ const nextConfig = {
     NEXT_PUBLIC_HOST_API: process.env.NEXT_PUBLIC_HOST_API,
   },
   images: {
-    domains: [process.env.NEXT_PUBLIC_HOST_DOMAIN, "https://dcatrah.com"],
+    domains: [process.env.NEXT_PUBLIC_HOST_DOMAIN,'https://dcatrah.com',"https://www.pexels.com"],
     unoptimized: true,
-    remotePatterns: [
-      {
-          protocol: 'http',
-          hostname: process.env.NEXT_PUBLIC_HOST_DOMAIN,
-          port: '',
-          pathname: '/storage/**',
-      },
-  ],
   },
 };
 
