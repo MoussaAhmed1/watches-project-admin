899ae3efc78ee081ef8fd75c899ca46309d2f195
Mousa Ahmed <moussa.abdelghany@gmail.com>
2024-06-27 23:12:44 +0300

    test-vercel

--- app/[lang]/dashboard/doctors/page.tsx
+++ app/[lang]/dashboard/doctors/page.tsx
@@ -36,7 +36,7 @@ export default async function page({ searchParams }: paramsProps) {
   const doctors: Employee[] = res?.data?.data || [];
   return (
     <>
-      <div className="flex-1 space-y-4  p-4 md:p-8 pt-6">
+      <div className="flex-1 space-y-4  p-4 md:p-8 pt-6 ">
         <BreadCrumb items={breadcrumbItems} />
 
         <div className="flex items-start justify-between">
