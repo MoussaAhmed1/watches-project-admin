3df511a101b32f09744ba6e18983b27abb66451c
Mousa Ahmed <moussa.abdelghany@gmail.com>
2024-06-30 14:53:56 +0300

    remove pharamcy/new

--- app/[lang]/dashboard/pharmacies/page.tsx
+++ app/[lang]/dashboard/pharmacies/page.tsx
@@ -42,7 +42,7 @@ export default async function page({ searchParams }: paramsProps) {
             title={`Pharmacies (${totalPharmacies})`}
           />
           <Link
-            href={"/dashboard/pharmacy/new"}
+            href={"/dashboard/pharmacies/new"}
             className={cn(buttonVariants({ variant: "default" }))}
           >
             <Plus className="mr-2 h-4 w-4" /> Add New
