c8063c415fb5d97509141dc145e58e389d7b84f8
Mousa Ahmed <moussa.abdelghany@gmail.com>
2024-07-15 16:54:18 +0300

    feat/messages

--- components/tables/settings/columns.tsx
+++ components/tables/settings/columns.tsx
@@ -2,6 +2,9 @@
 
 import { ColumnDef } from "@tanstack/react-table";
 import { SuggestionsComplaints } from "@/types/suggestions-complaints";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { AvatarImage } from "@radix-ui/react-avatar";
+import { CellAction } from "./cell-action";
 
 const columns: ColumnDef<SuggestionsComplaints>[] = [
   {
@@ -16,6 +19,37 @@ const columns: ColumnDef<SuggestionsComplaints>[] = [
       <div className="stars flex">{row?.original?.description}</div>
     ),
   },
-
+  {
+    accessorKey: "name",
+    header: "Name",
+    cell: ({ row }) => <div className="flex items-center gap-3">
+      <Avatar className="w-10 h-10">
+        <AvatarImage
+          src={row?.original?.user?.avatar ?? ""}
+          alt={row?.original?.user?.name ?? ""}
+        />
+        <AvatarFallback>{row?.original?.user?.name[0]}</AvatarFallback>
+      </Avatar>
+      <div className="flex flex-col">
+        <p >
+          {row?.original?.user?.name ?? ""}
+        </p>
+        <p >
+          {row?.original?.user?.phone ?? ""}
+        </p>
+      </div>
+    </div>
+  },
+  {
+    accessorKey: "Email",
+    header: "Email",
+    cell: ({ row }) => (
+      <div className="stars flex">{row?.original?.email ?? " - "}</div>
+    ),
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => <CellAction data={row.original} />,
+  },
 ];
 export default columns;
