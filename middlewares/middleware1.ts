b1eac7fa4e88ba213bc082ea6601ed894ae9780f
Mousa Ahmed <moussa.abdelghany@gmail.com>
2024-07-23 13:32:23 +0300

    Update middleware1.ts

--- middlewares/middleware1.ts
+++ middlewares/middleware1.ts
@@ -43,7 +43,7 @@ export function withAuthMiddleware(middleware: CustomMiddleware) {
 
     const apiToken = cookies()?.get("access_token")?.value;
     const NextsessionToken = cookies()?.get("next-auth.session-token")?.value;
-    if (!(NextsessionToken && apiToken && token)  && (protectedPathsWithLocale.includes(pathname) || pathname.includes("/dashboard"))) {
+    if (!(apiToken && token)  && (protectedPathsWithLocale.includes(pathname) || pathname.includes("/dashboard"))) {
       const signInUrl = new URL('/api/auth/signin', request.url)
       signInUrl.searchParams.set('callbackUrl', pathname)
       return NextResponse.redirect(signInUrl)
