68c0a85f5af3948ac4fea3527e723c381cf476a0
Mousa Ahmed <moussa.abdelghany@gmail.com>
2024-07-31 13:14:41 +0300

    fix/permissions

--- app/[lang]/dashboard/(users)/patients/[id]/edit/layout.tsx
+++ app/[lang]/dashboard/(users)/patients/[id]/edit/layout.tsx
@@ -57,7 +57,7 @@ export default async function updateLayout({ children, params }: updateLayoutPro
         </div>
         <Separator className="my-6" />
         <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
-          <aside className="-mx-4 lg:w-1/5">
+          <aside className="lg:w-1/5  w-full">
             <SidebarNav items={sidebarNavItems} />
           </aside>
           <div className="flex-1 ">{children}</div>
