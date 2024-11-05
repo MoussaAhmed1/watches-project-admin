0c503fffa11c4f870fb7950348c5b76992f9fed8
Mousa Ahmed <moussa.abdelghany@gmail.com>
2024-11-05 06:51:03 +0200

    fix:solve too much redirect

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -2,11 +2,13 @@ import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { authOptions } from "../api/auth/_options";
 import LoginForm from "@/components/forms/auth/login";
+import { cookies } from "next/headers";
 
 export default async function Home({ params }: { params: { lang: "ar" | "en" } }) {
   const session = await getServerSession(authOptions);
+  const apiToken = cookies()?.get("access_token")?.value;
 
-  if (session) {
+  if (session && apiToken) {
     redirect("/dashboard");
   }
   return <LoginForm  lang={params.lang} />
