dd208c73abed46fd475c423a3e162c50d8f7639d
Mousa Ahmed <moussa.abdelghany@gmail.com>
2024-11-07 17:32:33 +0200

    fix: :anger:  build

--- components/tables/suggestions/cell-action.tsx
+++ components/tables/suggestions/cell-action.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { AlertModal } from "@/components/modal/alert-modal";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
@@ -8,34 +7,22 @@ import {
   DropdownMenuLabel,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { PharmacyCategories } from "@/types/pharmacy-categories";
-import { Edit, MoreHorizontal, Trash, Eye } from "lucide-react";
+import { SuggestionsComplaints } from "@/types/suggestions/suggestions-complaints";
+import { MoreHorizontal, Eye, Reply } from "lucide-react";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
 import Cookie from 'js-cookie';
 import { useTranslations } from "next-intl";
 
 interface CellActionProps {
-  data: PharmacyCategories;
+  data: SuggestionsComplaints;
 }
 
 export const CellAction: React.FC<CellActionProps> = ({ data }) => {
-  const [loading, setLoading] = useState(false);
-  const [open, setOpen] = useState(false);
   const router = useRouter();
   const currentLang = Cookie.get("Language") ?? "en";
-  const t = useTranslations("tableActions");
-
-  const onConfirm = async () => {};
-
+ const t = useTranslations("tableActions");
   return (
     <>
-      <AlertModal
-        isOpen={open}
-        onClose={() => setOpen(false)}
-        onConfirm={onConfirm}
-        loading={loading}
-      />
       <DropdownMenu modal={false} dir={currentLang === "ar" ? "rtl" : "ltr"}>
         <DropdownMenuTrigger asChild>
           <Button variant="ghost" className="h-8 w-8 p-0">
@@ -46,17 +33,14 @@ export const CellAction: React.FC<CellActionProps> = ({ data }) => {
         <DropdownMenuContent align="end">
           <DropdownMenuLabel>{t("actions")}</DropdownMenuLabel>
           <DropdownMenuItem
-            onClick={() => router.push(`/dashboard/doctors/${data.id}`)}
+            onClick={() => router.push(`/${currentLang}/dashboard/messages/${data.id}`)}
           >
             <Eye className="mx-1 h-4 w-4"/> {t("view")}
           </DropdownMenuItem>
           <DropdownMenuItem
-            onClick={() => router.push(`/dashboard/doctors/${data.id}`)}
+            onClick={() => router.push(`/${currentLang}/dashboard/doctors/${data.id}`)}
           >
-             <Edit className="mx-1 h-4 w-4" /> {t("update")}
-          </DropdownMenuItem>
-          <DropdownMenuItem onClick={() => setOpen(true)}>
-            <Trash className="mx-1 h-4 w-4" /> {t("delete")}
+            <Reply  className="mx-1 h-4 w-4" /> Repley
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
