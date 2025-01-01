f9a60cef1edf0e41cf77ffebcd252120364a871d
Mousa Ahmed <moussa.abdelghany@gmail.com>
2025-01-01 23:20:32 +0200

    login kabab-case

--- components/forms/auth/login.tsx
+++ components/forms/auth/login.tsx
@@ -60,7 +60,7 @@ export default function LoginForm({ lang }: { lang: Language }) {
                                 <button
                                     className="mt-5 tracking-wide font-semibold bg-[#027BA7] text-gray-100 w-full py-4 rounded-lg hover:bg-[#027BA7] transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
                                     <svg className="w-6 h-6 -ml-2" fill="none" stroke="currentColor" strokeWidth="2"
-                                        stroke-linecap="round" stroke-linejoin="round">
+                                        strokeLinecap="round" strokeLinejoin="round">
                                         <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
                                         <circle cx="8.5" cy="7" r="4" />
                                         <path d="M20 8v6M23 11h-6" />
