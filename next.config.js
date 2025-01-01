20e3f3d4cf0b5b6b2461ba2fc4ea07cf6b38567c
Mousa Ahmed <moussa.abdelghany@gmail.com>
2025-01-01 23:50:20 +0200

    deploy

--- next.config.js
+++ next.config.js
@@ -1,14 +1,34 @@
 /** @type {import('next').NextConfig} */
 const withNextIntl = require('next-intl/plugin')();
 
+
 const nextConfig = withNextIntl({
   env: {
     NEXT_PUBLIC_HOST_API: process.env.NEXT_PUBLIC_HOST_API,
   },
   images: {
-    domains: [process.env.NEXT_PUBLIC_HOST_DOMAIN,"https://nadnee.click/v1/"],
+    domains: [process.env.NEXT_PUBLIC_HOST_DOMAIN,"https://nadnee.click/v1/","nadnee.click"],
     unoptimized: true,
+  },
+  modularizeImports: {
+    '@mui/icons-material': {
+      transform: '@mui/icons-material/{{member}}',
+    },
+    '@mui/material': {
+      transform: '@mui/material/{{member}}',
+    },
+    '@mui/lab': {
+      transform: '@mui/lab/{{member}}',
+    },
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
   },
 });
 
 module.exports = nextConfig;
+
